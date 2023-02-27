5e621b6bbcae96851a819467b2ce07361e376477
delver <github@delver.zone>
2023-02-27 21:10:16 +0000

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat) {
+  let concatString = "";
+  if (repeat < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < repeat; i++) {
+    concatString += word;
+  }
+  return concatString;
 };
 
 // Do not edit below this line
